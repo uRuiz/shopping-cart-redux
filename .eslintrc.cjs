5a7fd8f417133629107d78a59a16a6abffe18c35
uRuiz <zaszur@gmail.com>
2023-12-05 12:03:35 +0100

    Add prettier. Add images. Add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,24 +3,27 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: 'airbnb',
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: ['.eslintrc.{js,cjs}'],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
+  plugins: ["react"],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    'import/no-extraneous-dependencies': 'off',
+    "react/react-in-jsx-scope": "off",
+    "import/no-extraneous-dependencies": "off",
+    "no-unused-vars": "off",
+    "react/prop-types": "off",
   },
 };
